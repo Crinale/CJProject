140fc05641f7295c550947b3da1ae142fff865ea
Devon Chu <devonchu@hotmail.com>
2015-08-13 17:18:23 -0700

    First upload able to read CSV files.

---
+++ CSVproject.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+int fileStuff();
+
+int main(){
+
+fileStuff();
+
+
+}
+
+
+// file opening stuff so that it can be used in putPoint.......
+  int fileStuff(){
+        ifstream file("test.csv");
+        string value;
+			while (!file.eof()){
+
+				getline(file,value,',');
+				value = value.length/3;
+				cout << value;
+
+			}
+
+return 0;
+		}
+/*
+		//this part is for putting points into open gl
+ 		putPoint(int a,int b, int c){
+
+
+ 		}
+*/
