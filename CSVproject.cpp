5768ab8508dde625945429bebe9e89219f566856
Devon Chu <devonchu@hotmail.com>
2015-08-17 16:15:52 -0700

    Fixed queue able to read from second function

--- CSVproject.cpp
+++ CSVproject.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
+
 int fileStuff();
-
-
+int queueThingy(int x1,int y1,int z1);
 int main(){
 
 fileStuff();
@@ -21,17 +21,36 @@ fileStuff();
     int fileStuff(){
         ifstream file("test.csv");
         string value;
+        int number;
+        queue< int > arr;
 			while (!file.eof()){
-
 				getline(file,value,',');
-				int number;
 				istringstream (value) >> number;
-				queue< int > arr;
                 arr.push(number);
-                cout << arr.front() << endl ;
-			}
+   			}
+
+   			for(int i= 0; i < 4 ; i++){
+                int x = arr.front();
+                arr.pop();
+                int y = arr.front();
+                arr.pop();
+                int z = arr.front();
+                arr.pop();
+                queueThingy(x,y,z);
+   			}
+
+
+
     return 0;
   }
+
+
+
+
+int queueThingy(int x1,int y1,int z1){
+cout << x1<<y1<<z1<<endl;
+}
+
 
 /*
 		//this part is for putting points into open gl
