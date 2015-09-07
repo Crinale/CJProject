0fc15e1be209e664b24546d99a7154c39339a25e
Devon Chu <devonchu@hotmail.com>
2015-09-07 00:18:10 -0700

    Clean Version of OpenGl now able to go past a box of (1,-1)(1,-1)(1,-1) Next on Todo list is Camera Control then porting numbers into our thingy

--- CSVproject.cpp
+++ CSVproject.cpp
@@ -8,7 +8,6 @@
 #else
 #include <GL/glut.h>
 #endif
-
 #include <stdlib.h>
 
 
@@ -17,26 +16,25 @@ using namespace std;
 
 int fileStuff();
 int queueThingy(int x1,int y1,int z1);
-
-
-static void display(void)
-{
-
-}
-
-
+static void display(void);
+void reshape(int width,int height);
 
 
 int main(int argc,char **argv){
     fileStuff();
-
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("IT WORKS!");
-    glutDisplayFunc(display);
-    glutMainLoop();
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_RGB);
+	glutCreateWindow("CSV Thingy");
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-5.0,5.0,-5.0,5.0,-5.0,5.0);
+	glClearColor(0.5, 0.5, 0.5, 0.5);
+	glutMainLoop();
+	return 0;
 
     return EXIT_SUCCESS;
 
@@ -83,3 +81,24 @@ cout << x1<<y1<<z1<<endl;
 
  		}
 */
+
+
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glBegin(GL_TRIANGLES);
+    glColor3f(.1,.2,.3);
+    glVertex3f(-1,0,0);
+    glVertex3f(1,0,0);
+    glVertex3f(0,1,0);
+    glEnd();
+    //glColor4f(1.0, 0.0, 0.0, 1.0);
+    //glutSolidTeapot(2);
+    glFlush();
+}
+
+void reshape(int width, int height)
+{
+    glViewport(0,0,width,height);
+}
